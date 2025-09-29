5a3c408da925024b983d737b5d2aac6e6b85a959
ivana-antunovic <ivana.antunovic.ri@gmail.com>
2025-09-29 20:34:39 +0200

    player je dobio supermoci

--- New Input/Assets/Scripts/InputSystem/PlayerMovement.cs
+++ New Input/Assets/Scripts/InputSystem/PlayerMovement.cs
@@ -1,22 +1,29 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // trenutno player moze uzeti objekat gdje god se nalazio, krenula sam raditi metodu za neki pickup range ali odjednom mi
+    //se unity crashao tako da je cijeli ekran bio u sarenim crtama i onda se sve zacrnilo. zatim se pojavila kockica sa logom
+    //unityja i crvenim usklicnikom. ugl nakon te epizode sam odlucila da player ima neku telekinezu i moze manipulirati predmetima
+    //iz daljine
     private Rigidbody rigidbody;
+
+    [SerializeField] private GameObject selectedObject;
+    [SerializeField] private Vector3 pickedUpPosition;
+    [SerializeField] private float pickupRange = 2f;
 
     private Vector2 moveInput;
     [SerializeField] private float moveSpeed = 10f;
-    [SerializeField] private float crouchSpeed = 5f;
-
 
     private Vector2 lookInput;
 
-    public bool crouch;
+    private bool isTrigger;
+    int counter = 0;
 
-   
-
-
+    private bool isCrounching;
+    private bool isPickedUp;
 
     private void Awake()
     {
@@ -31,8 +38,50 @@ public class PlayerMovement : MonoBehaviour
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        Debug.Log(moveInput);
     }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isTrigger = true;
+        }
+
+        if (context.canceled)
+        {
+            isTrigger = false;
+        }
+    }
+
+    public void OnCrouch(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isCrounching = true;
+        }
+
+        if (context.canceled)
+        {
+            isCrounching = false;
+        }
+
+        Crouch();
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if(context.started)
+        {
+           PickUpObject();
+        }
+    }
+
+
 
     private void Move()
     {
@@ -41,53 +90,8 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 moveDirection = forward * moveInput.y + right * moveInput.x;
 
-       // Vector3 velocity = new Vector3(moveInput.x, 0, moveInput.y);
+        // Vector3 velocity = new Vector3(moveInput.x, 0, moveInput.y);
         rigidbody.linearVelocity = moveDirection * moveSpeed;
-    }
-
-    public void OnShoot(InputAction.CallbackContext context)
-    {
-       if(context.started)
-       {
-         Shoot();
-       }
-    }
-
-    public void OnCrouch(InputAction.CallbackContext context)
-    {
-         if(context.started)
-         {
-            crouch = true;
-         }
-
-         if(context.started)
-         {
-            crouch = false;
-         }
-
-         Crouch();
-    }
-
-    private void Crouch()
-    {
-        if (crouch)
-        {
-            moveSpeed = crouchSpeed;
-            transform.localScale = new Vector3(1, 0.5f, 1);
-            transform.localPosition = new Vector3(transform.localPosition.x, 0.5f, transform.localPosition.y);
-        }
-
-        if(!crouch)
-        {
-            moveSpeed = 10f;
-            transform.localScale = Vector3.one;
-        }
-    }
-
-
-    private void Shoot()
-    {
-        Debug.Log($"Shoot");
     }
 
     private void Look()
@@ -95,31 +99,72 @@ public class PlayerMovement : MonoBehaviour
         transform.Rotate(Vector3.up, lookInput.x);
     }
 
-    public void OnLook(InputAction.CallbackContext context)
-
-    {
-        lookInput = context.ReadValue<Vector2>();
-        Debug.Log(lookInput);
-    }
-
-    public void OnInteract()
+    private void Attack()
     {
 
+
+        if (isTrigger)
+        {
+            counter++;
+            Debug.Log($"I attacked {counter} times");
+        }
     }
 
-    private void Interact()
+    private void Crouch()
     {
+        if (isCrounching)
+        {
+            moveSpeed = 5.5f;
+            transform.localScale = new Vector3(1, 0.5f, 1);
+            transform.localPosition = new Vector3(transform.localPosition.x, 0.5f, transform.localPosition.z);
+        }
 
+        if (!isCrounching)
+        {
+            moveSpeed = 10;
+            transform.localScale = Vector3.one;
+        }
     }
 
+    private void PickUpObject()
+    {
+        if (!isPickedUp && selectedObject != null)
+        {
+            
+            isPickedUp = true;
+            selectedObject.transform.SetParent(transform);
+            selectedObject.transform.localPosition = pickedUpPosition;
+
+            Rigidbody objRigidbody = selectedObject.GetComponent<Rigidbody>();
+            if (objRigidbody != null)
+            {
+                objRigidbody.isKinematic = true;
+            }
+        }
+        else if (isPickedUp)
+        {
+           
+            isPickedUp = false;
+            selectedObject.transform.SetParent(null);
+
+            Rigidbody objRigidbody = selectedObject.GetComponent<Rigidbody>();
+            if (objRigidbody != null)
+            {
+                objRigidbody.isKinematic = false;
+            }
+        }
+    }
+
+   
 
 
     private void Update()
     {
         Move();
         Look();
-        Crouch();
-        Interact();
-        
+        Attack();
+        // Crouch();
+       
+
     }
 }
